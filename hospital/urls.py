e166d4fabc5e0b08d2fd6d9cc5a601fb23386227
Aditya Tiwari <tiwariaditya28925@gmail.com>
2026-08-20 05:59:33 +0530

    Add detail page routes

--- hospital/urls.py
+++ hospital/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("departments/", views.departments, name="departments"),
+    path("departments/<int:department_id>/", views.department_detail, name="department_detail"),
     path("doctors/", views.doctors, name="doctors"),
+    path("doctors/<int:doctor_id>/", views.doctor_detail, name="doctor_detail"),
     path("gallery/", views.gallery, name="gallery"),
     path("contact/", views.contact, name="contact"),
     path("appointments/book/", views.book_appointment, name="book_appointment"),
